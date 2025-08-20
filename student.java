511f72d5c5522886ca31bca06b1d8201769aa5d5
shailasnigdha <141671612+shailasnigdha@users.noreply.github.com>
2025-08-20 03:45:08 -0700

    Add files via upload

---
+++ student.java
@@ -0,0 +1,14 @@
+public class student {
+    private String name;
+    private int age;
+    public student (String name , int age)  {
+        this.name=name;
+        this.age=age;
+    }
+    public String getname(){
+        return name;
+    }
+    public void setname(String name){
+        this.name=name;
+    }
+}
